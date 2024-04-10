4db7112dc4ec62ae8585d94450c8d90d7fd70f3b
Esraa156 <eo6344848@gmail.com>
2024-04-10 02:29:45 +0200

    ProductConfigurations ,Migrations

--- TalabatProject.APIs/Program.cs
+++ TalabatProject.APIs/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Talabat.Repository.Data;
+
 namespace Talabat.APIs
 {
 	public class Program
@@ -19,6 +22,16 @@ namespace Talabat.APIs
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webApplicationBuilder.Services.AddEndpointsApiExplorer();
 			webApplicationBuilder.Services.AddSwaggerGen();
+			webApplicationBuilder.Services.AddDbContext<StoreContext>(options=>
+			{
+				options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+
+
+
+
+
+				});
+
 
 			#endregion
 			var app = webApplicationBuilder.Build();
