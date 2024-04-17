6dde4dd1f3d2d019d582baa1e04735c8474ead73
Esraa156 <eo6344848@gmail.com>
2024-04-17 15:05:58 +0200

    Employee Example "Draft"

---
+++ Talabat.Core/EmployeeSpecification/EmployeeWithDepartmentSpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
+
+namespace Talabat.Core.EmployeeSpecification
+{
+	public class EmployeeWithDepartmentSpecification:BaseSpecifications<Employee>
+	{
+		public EmployeeWithDepartmentSpecification() 
+			: base()
+		{
+			Includes.Add(E => E.department);
+		}
+		public EmployeeWithDepartmentSpecification(int id) 
+			: base(E=>E.Id==id)
+		{
+			Includes.Add(E => E.department);
+		}
+
+	}
+}
---
+++ TalabatProject.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.EmployeeSpecification;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Specifications.ProductSpecification;
+
+namespace TalabatProject.APIs.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		private readonly IGenericRepository<Employee> _employeerepo;
+
+		public EmployeeController(IGenericRepository<Employee> employeerepo)
+		{
+			_employeerepo = employeerepo;
+		}
+		[HttpGet] //GET  /api/Employees
+
+		public async Task<ActionResult<IEnumerable<Employee> >>GetEmployees() 
+		{
+			
+			var spec =new EmployeeWithDepartmentSpecification();
+			var employees = await _employeerepo.GetAllAsyncSpec(spec);
+			return Ok(employees);
+
+		}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Employee>> GetEmployee(int id)
+		{
+			var spec = new EmployeeWithDepartmentSpecification(id);
+
+			var employee = await _employeerepo.GetAsyncSpec(spec);
+
+			if (employee is null)
+				return NotFound();
+			return Ok(employee); //200
+		}
+	}
+}
