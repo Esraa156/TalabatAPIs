ca287bbfbdf17d0f6e6bcafef2bb7778c4a8eebd
Esraa156 <eo6344848@gmail.com>
2024-04-19 15:03:56 +0200

    Handling NotFounf Endpoint

---
+++ TalabatProject.APIs/Controllers/ErrorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using TalabatProject.APIs.Errors;
+
+namespace TalabatProject.APIs.Controllers
+{
+
+	[Route("errors/{code}")]
+	[ApiController]
+	[ApiExplorerSettings(IgnoreApi =true)]
+	public class ErrorsController:ControllerBase
+	{
+		public ActionResult Error(int code)
+		{
+			if (code == 404)
+				return NotFound(new ApiResponse(404));
+			else if (code == 401)
+				return Unauthorized(new ApiResponse(404));
+			else
+				return StatusCode(code);
+
+		}
+
+	}
+}
--- TalabatProject.APIs/Program.cs
+++ TalabatProject.APIs/Program.cs
@@ -137,6 +137,7 @@ namespace Talabat.APIs
 				app.UseSwaggerUI();
 			}
 
+			app.UseStatusCodePagesWithReExecute("/errors/{0}");
 			app.UseHttpsRedirection();
 
 			app.UseAuthorization();
