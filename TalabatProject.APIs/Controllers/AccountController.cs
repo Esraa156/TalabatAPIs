d012ccc663b66b0a777476c7a644f78f8a3409ca
Esraa156 <eo6344848@gmail.com>
2024-04-27 23:51:56 +0300

    Register Endpoint

--- TalabatProject.APIs/Controllers/AccountController.cs
+++ TalabatProject.APIs/Controllers/AccountController.cs
@@ -15,9 +15,9 @@ namespace TalabatProject.APIs.Controllers
 		private readonly SignInManager<ApplicationUser> _signInManager;
 
 		public AccountController(
-            UserManager<ApplicationUser>userManager,
-            SignInManager<ApplicationUser>signInManager)
-        {
+			UserManager<ApplicationUser> userManager,
+			SignInManager<ApplicationUser> signInManager)
+		{
 			_userManager = userManager;
 			_signInManager = signInManager;
 		}
@@ -26,15 +26,15 @@ namespace TalabatProject.APIs.Controllers
 		public async Task<ActionResult<UserDto>> Login(LoginDto model)
 
 		{
-			var user=await _userManager.FindByEmailAsync(model.Email);
-			if(user is  null)
+			var user = await _userManager.FindByEmailAsync(model.Email);
+			if (user is null)
 			{
-				return Unauthorized(new ApiResponse(401,"Invalid Login"));
-					
+				return Unauthorized(new ApiResponse(401, "Invalid Login"));
+
 			}
 			else
 			{
-				var Result = await _signInManager.CheckPasswordSignInAsync(user, model.Password,false);
+				var Result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
 				if (!Result.Succeeded)
 				{
 					return Unauthorized(new ApiResponse(401, "Invalid Login"));
@@ -49,18 +49,38 @@ namespace TalabatProject.APIs.Controllers
 						Token = "This will be token"
 
 
-					}) ;
+					});
 				}
+			}
+		}
+		[HttpPost("register")] //POST: /api/Account/register
 
+		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+		{
+			var user = new ApplicationUser()
+			{
+				DisplayName = model.DispalyName,
+				Email = model.Email,
+				UserName = model.Email.Split("@")[0],
+				PhoneNumber = model.Phone
+			};
+			var result=await _userManager.CreateAsync(user,model.Password);
+			if (!result.Succeeded)
+			
+				return BadRequest(new ApiValidationErrorResponse(){Errors=result.Errors.Select(E=>E.Description)});
+
+			
+				return Ok(new UserDto
+				{
+					DisplayName = user.DisplayName,
+					Email = user.Email,
+					Token = "This will be token"
+
+
+				});
 			}
 		}
 
 
-
-
-
-
-
-
-    }
-}
+	}
+	
---
+++ TalabatProject.APIs/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalabatProject.APIs.DTOs
+{
+	public class RegisterDto
+	{
+		[Required]
+		public string DispalyName { get; set; } = null!;
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = null!;
+		[Required]
+		[RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{\":;'?/>.<,])(?!.*\\s).*",
+		   ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non-alphanumeric and at least 6 characters")]
+		public string Password { get; set; } = null!;
+		[Required]
+		public string Phone { get; set; } = null!;
+
+	}
+}
