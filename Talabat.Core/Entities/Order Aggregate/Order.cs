38d51f2ccc7580cd70db5e0a066110a1a8d4d3a6
Esraa156 <eo6344848@gmail.com>
2024-05-04 21:20:40 +0300

    Order Module Entities

---
+++ Talabat.Core/Entities/Order Aggregate/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+	public class Order
+	{
+		public string BuyerEmail { get; set; } = null!;
+		 
+		public DateTimeOffset OrderDate { get; set; }= DateTimeOffset.UtcNow;
+
+		public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+		public Address ShippingAddress { get; set; }= null!;
+
+		public int DeliveryMethodId {  get; set; }	 // Foreign Key
+		
+		public DeliveryMethod DeliveryMethod { get; set; } = null!; //Navigational Property [ONE]
+
+
+		public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>(); //Navigational Property	
+
+		public decimal SubTotal {  get; set; }
+
+		//[NotMapped]
+		//public decimal  Total =>SubTotal + DeliveryMethod.Cost; 
+
+
+		public decimal GetTotal()=> SubTotal + DeliveryMethod.Cost;
+
+		public string PaymentIntentId { get; set;} = string.Empty;
+
+
+
+
+
+	}
+}
